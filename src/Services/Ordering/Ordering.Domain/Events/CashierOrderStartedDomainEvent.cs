39a8d624ebb237800cb8a30339f6309c72c9349b
Moein Shafiei <mrshafiei@hotmail.com>
2023-07-21 22:12:19 -0400

    add cashier order view

---
+++ src/Services/Ordering/Ordering.Domain/Events/CashierOrderStartedDomainEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Ordering.Domain.Events;
+public class CashierOrderStartedDomainEvent : INotification
+{
+    public Guid SessionID { get; set; }
+
+    public string CashierName { get; set; }
+
+    public string UserId { get; }
+    public string UserName { get; }
+}
---
+++ src/Web/WebMVC/ViewModels/Cashier/Cashier.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.eShopOnContainers.WebMVC.ViewModels;
+
+public class Cashier
+{
+    public List<CatalogItem> Data { get; init; }
+
+    public string CashierName { get; set; }
+
+    public string SessionID { get; set; }
+}
